156e9cd2944808355c10c57e51849564d16faae3
prituladima <prituladima@gmail.com>
2017-09-06 16:26:59 +0300

    JPA and Hibernate integration

---
+++ src/main/java/com/prituladima/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.prituladima.repository;
+
+
+import com.prituladima.entities.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
